2d4e47bfc2e8109e143a3c9438042ef41a391b47
beeleza <higorbeleza.dev@gmail.com>
2025-11-14 10:09:06 -0300

    feat: basic satup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+};
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+ap.use(cookieParser());
 
 app.get("", (req, res) => {
   res.send("Hello world");
